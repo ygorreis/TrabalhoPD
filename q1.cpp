0fc3bd89d6772d4b44d983205c4bed3c9129798c
Ygor Reis <ygorreis@hotmail.com>
2015-06-23 02:27:05 -0300

    Novos arquivos, referentes ao cruzamento de DNA, foram adicionados.

--- q1.cpp
+++ q1.cpp
@@ -5,11 +5,15 @@
 
 //variáveis globais
 int tamanhoDaSequencia = 0, numeroAuxiliar = 0;
-char dna[256], base = 'A';
-char url[] = "C:\\Users\\Ygor Reis\\Documents\\trabalhoAuriLinda\\dna.txt", urlSaida[] = "C:\\Users\\Ygor Reis\\Documents\\trabalhoAuriLinda\\dna_saida.txt",digitos[2];
+char dna[256], cromossomo1[256], cromossomo2[256], base = 'A';
+char url[] = "C:\\Users\\Ygor Reis\\Documents\\trabalhoAuriLinda\\dna.txt", 
+urlSaida[] = "C:\\Users\\Ygor Reis\\Documents\\trabalhoAuriLinda\\dna_saida.txt",
+urlCromossomo1[] = "C:\\Users\\Ygor Reis\\Documents\\trabalhoAuriLinda\\cromossomo1.txt",
+urlCromossomo2[] = "C:\\Users\\Ygor Reis\\Documents\\trabalhoAuriLinda\\cromossomo2.txt",
+urlCromossomoSaida[] = "C:\\Users\\Ygor Reis\\Documents\\trabalhoAuriLinda\\cromossomo_saida.txt",digitos[2];
 
 //declaração de métodos
-void lerArquivo(char *url, char *digitos);
+void lerArquivo(char *url, char *digitos, int opcaoImpressao);
 void obterTamanhoSequencia(char *digitos);
 void criarCadeiaCorrespondente(char *dna);
 void salvarArquivo(char *url, char *dna);
@@ -17,19 +21,20 @@ void gerarMutacao(char *dna, char baseDesejada);
 void zerarNumeroAuxiliar();
 void criarNumeroRandomico();
 void imprimirResultados(int opcao);
+void gerarCruzamento();
 
 int main(){
 		
-	lerArquivo(url,digitos);	
+	lerArquivo(url,digitos,0);	
 	criarCadeiaCorrespondente(dna);
 	salvarArquivo(urlSaida,dna);
 	gerarMutacao(dna,base);
+	gerarCruzamento();
     
 	return 0;
 }
 
-void lerArquivo(char *url, char *digitos){
-	
+void lerArquivo(char *url, char *digitos, int opcaoImpressao){	
 	FILE *arq = fopen(url, "r");
 	if(arq == NULL){
 		printf("ERRO NA LEITURA");
@@ -45,7 +50,7 @@ void lerArquivo(char *url, char *digitos){
 		}
 	}
 	fclose(arq);
-	imprimirResultados(0);
+	imprimirResultados(opcaoImpressao);
 }
 
 void obterTamanhoSequencia(char *digitos){
@@ -91,8 +96,11 @@ void imprimirResultados(int opcao){
 		printf("DNA ORIGINAL       - %s\n", dna);	
 	}else if(opcao == 1){
 		printf("DNA CORRESPONDENTE - %s\n", dna);
-	}else{
+	}else if(opcao == 2){
 		printf("DNA COM MUTACAO    - %s\n", dna);
+	}else{
+		printf("\n");
+		printf("DNA CRUZADO        - %s\n", dna);
 	}
 }
 
@@ -102,7 +110,7 @@ void criarNumeroRandomico(){
 	srand(time(NULL));
 	numeroAuxiliar = rand() % tamanhoDaSequencia;
 	printf("\n");
-	printf("POSICAO DA BASE TROCADA: %d\n", numeroAuxiliar);
+	printf("POSICAO DA BASE TROCADA: %d\n", numeroAuxiliar + 1);
 }
 
 void gerarMutacao(char *dna, char baseDesejada){
@@ -126,4 +134,39 @@ void gerarMutacao(char *dna, char baseDesejada){
 
 void zerarNumeroAuxiliar(){
 	numeroAuxiliar = 0;
+}
+
+void gerarCruzamento(){
+	zerarNumeroAuxiliar();
+	lerArquivo(urlCromossomo1,digitos,3);
+	strncpy(cromossomo1, dna, tamanhoDaSequencia);
+	
+	zerarNumeroAuxiliar();
+	lerArquivo(urlCromossomo2,digitos,3);
+	strncpy(cromossomo2, dna, tamanhoDaSequencia);
+
+	criarNumeroRandomico();
+	
+	char* descendencia1 = (char *) malloc((tamanhoDaSequencia + 1) * sizeof(char));
+	char* descendencia2 = (char *) malloc((tamanhoDaSequencia + 1) * sizeof(char));
+	int auxiliar = 0;
+	
+	//copiando os primeiros numeros antes do ponto de cruzamento
+	//numeroAuxiliar == ponto do cruzamento
+	for(auxiliar; auxiliar < tamanhoDaSequencia; auxiliar++){
+		if(auxiliar < numeroAuxiliar){
+			strncpy(descendencia1,cromossomo1,auxiliar);
+			strncpy(descendencia2,cromossomo2,auxiliar);
+			//printf("DESCENDENCIA %d - %s \n", auxiliar, descendencia1);
+			//printf("DESCENDENCIA %d - %s \n", auxiliar, descendencia2);
+		}else{
+			strncpy(descendencia1,cromossomo2,auxiliar);
+			strncpy(descendencia2,cromossomo1,auxiliar);
+			//printf("DESCENDENCIA %d - %s \n", auxiliar, descendencia1);
+			//printf("DESCENDENCIA %d - %s \n", auxiliar, descendencia2);
+		}		
+	}	
+	
+	printf("DESCENDENCIA 01 - %s \n", descendencia1);
+	printf("DESCENDENCIA 02 - %s \n", descendencia2);
 }
---
+++ q2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+main()
+
+{
+
+char x[10] = "450";
+
+int result = atoi(x);
+
+printf("integer value of the string is %d\n", result);
+
+}
